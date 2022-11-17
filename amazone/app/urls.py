d63020dd69f0ccdfe385247d90c6e3fe2f5e2cd9
aamirpython <aamir.manihar53@gmail.com>
2022-11-17 15:14:08 +0530

    login error solved

--- amazone/app/urls.py
+++ amazone/app/urls.py
@@ -40,4 +40,5 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name= 'password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_confirm.html'), name= 'password_reset_confirm'),
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    path('accounts/profile/', views.show_cart)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
